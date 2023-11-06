44e5eb9b4debc34f34ad64da9e8d61bb31ff2e22
unknown <makarand.gijam22@gmail.com>
2023-11-06 11:34:37 +0530

    changed scripts

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection=mongoose.connect(process.env.mongoLink);
+const connection = mongoose.connect(process.env.mongoLink);
 
-module.exports={connection};
+module.exports = { connection };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express=require("express");
-const app=express();
-const cors=require("cors");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const { connection } = require("./db");
 const { userRouter } = require("./routes/users.routes");
 const { productRouter } = require("./routes/course.routes");
@@ -8,19 +8,18 @@ const { productRouter } = require("./routes/course.routes");
 //internal middlewares..................
 app.use(cors());
 app.use(express.json());
-app.use("/users",userRouter);
-app.use("/courses",productRouter);
+app.use("/users", userRouter);
+app.use("/courses", productRouter);
 
 // app.get("/",(req,res)=>{
 //     res.send("server is working");
 // })
 
-app.listen(8080,async()=>{
-    try{
-        await connection;
-        console.log("server is running at port 8080");
-    }
-    catch(err){
-        console.log("server error");
-    }
-})
+app.listen(8080, async () => {
+  try {
+    await connection;
+    console.log("server is running at port 8080");
+  } catch (err) {
+    console.log("server error");
+  }
+});
